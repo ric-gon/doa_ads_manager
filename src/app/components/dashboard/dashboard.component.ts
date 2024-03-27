e96a91787ec7c06bb018879efaaabdddd1bbe67b
ric-gon <ricardogonzalez5674@gmail.com>
2024-03-26 21:42:01 -0500

    lightweight-charts library added

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { ColorType, DeepPartial, TimeChartOptions, createChart } from 'lightweight-charts';
 import { FacebookApiService } from '../../services/facebook-api.service';
 
 @Component({
@@ -8,13 +9,78 @@ import { FacebookApiService } from '../../services/facebook-api.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  data : string = 'No data';
-  response: string = 'No Response';
-  
-  constructor(private facebookApiService: FacebookApiService){}
+export class DashboardComponent implements AfterViewInit {
+  @ViewChild('firstChart') firstChart!: ElementRef;
+  chartOptions: DeepPartial<TimeChartOptions> =
+    {
+      layout:
+      {
+        textColor: 'white', background:
+        {
+          type: ColorType.Solid, color: 'black'
+        }
+      }
+    };
 
-  getFacebookData(){
+  data: string = 'No data';
+  response: string = 'No Response';
+
+  constructor(private facebookApiService: FacebookApiService) {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.getChart();
+  }
+
+
+  getFacebookData() {
     this.facebookApiService.getAdAccountData();
+  }
+
+  getChart(){
+    let chart = createChart(this.firstChart!.nativeElement, this.chartOptions);
+    console.log("document?: ", this.firstChart!.nativeElement)
+
+    let areaSeries = chart.addAreaSeries({
+      lineColor: '#2962FF', 
+      topColor: '#2962FF',
+      bottomColor: 'rgba(41, 98, 255, 0.28)',
+    });
+    let candlestickSeries = chart.addCandlestickSeries({
+      upColor: '#26a69a', 
+      downColor: '#ef5350', 
+      borderVisible: true,
+      wickUpColor: '#26a69a', 
+      wickDownColor: '#ef5350',
+    });
+
+    areaSeries.setData([
+      { time: '2018-12-22', value: 32.51 },
+      { time: '2018-12-23', value: 31.11 },
+      { time: '2018-12-24', value: 27.02 },
+      { time: '2018-12-25', value: 27.32 },
+      { time: '2018-12-26', value: 25.17 },
+      { time: '2018-12-27', value: 28.89 },
+      { time: '2018-12-28', value: 25.46 },
+      { time: '2018-12-29', value: 23.92 },
+      { time: '2018-12-30', value: 22.68 },
+      { time: '2018-12-31', value: 22.67 },
+    ]);
+
+    candlestickSeries.setData([
+      { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+      { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
+      { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
+      { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
+      { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
+      { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
+      { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
+      { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
+      { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
+      { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
+    ]);
+
+    chart.timeScale().fitContent();
   }
 }
